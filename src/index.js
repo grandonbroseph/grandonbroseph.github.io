02530fe387ac395ca6cf6e1aee499349b3614e4e
Brandon Semilla <semibran@gmail.com>
2021-01-04 19:56:26 -0800

    style and structure tweaks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import patch from '@semibran/patch'
-import { main } from './lib/vdom'
+import * as h from './lib/vdom'
 import Project from './views/project'
 import * as projects from './projects'
 
 const $root = document.querySelector('main')
 
-patch($root, Project(projects.tactics))
+patch($root, h.main([
+  h.header({ class: 'header' }, [
+    h.div({ class: 'back' }, [
+      h.span({ class: 'back-icon' })
+    ])
+  ]),
+  Project(projects.tactics)
+]))
--- src/views/Project.js
+++ src/views/Project.js
@@ -1,30 +1,30 @@
-import { div, article, aside, h1, h2, p, img } from '../lib/vdom'
+import * as h from '../lib/vdom'
 
 export default function Project(project) {
   const selectidx = 0
   const selectsrc = project.images[selectidx]
-  return div({ class: 'project' }, [
-    article({ class: 'project-content' }, [
-      div({ class: 'project-carousel' },
+  return h.div({ class: 'project' }, [
+    h.article({ class: 'project-content' }, [
+      h.div({ class: 'project-carousel' },
         project.images.map((src, idx) =>
           idx === selectidx
-            ? img({ class: 'carousel-image -select', src })
-            : img({ class: 'carousel-image', src })
+            ? h.img({ class: 'carousel-image -select', src })
+            : h.img({ class: 'carousel-image', src })
       )),
-      div({ class: 'project-header' }, [
-        div({ class: 'project-headings' }, [
-          h1({ class: 'project-title'}, [project.name]),
-          h2({ class: 'project-lifespan' },
+      h.div({ class: 'project-header' }, [
+        h.div({ class: 'project-headings' }, [
+          h.h1({ class: 'project-title'}, [project.name]),
+          h.h2({ class: 'project-lifespan' },
             [`${project.lifespan[0]} - ${project.lifespan[1]}`])
         ]),
       ]),
-      ...project.content.map(text => p([text]))
+      ...project.content.map(text => h.p([text]))
     ]),
-    aside({ class: 'project-preview' }, [
-      div({ class: 'preview-imagewrap' }, [
-        img({ class: 'preview-image', src: selectsrc }),
-        div({ class: 'preview-refl' }, [
-          img({ class: 'preview-image', src: selectsrc }),
+    h.aside({ class: 'project-preview' }, [
+      h.div({ class: 'preview-imagewrap' }, [
+        h.img({ class: 'preview-image', src: selectsrc }),
+        h.div({ class: 'preview-refl' }, [
+          h.img({ class: 'preview-image', src: selectsrc }),
         ])
       ])
     ])
