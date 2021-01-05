2120dab30d4d5444c121cece9e03c1ef66aced7e
Brandon Semilla <semibran@gmail.com>
2021-01-04 20:37:01 -0800

    tweak project description

--- src/projects.js
+++ src/projects.js
@@ -5,6 +5,6 @@ export const tactics = {
   lifespan: [2018, 2020],
   content: [
     'In this game, the player\'s army faces off against the enemy in turn-based combat. The game ends once the commander on either side is defeated.',
-    'This project has been one of my first forays into integrating UI mockups into a game. I\'ve learned to develop state-heavy, animation-driven, and scalable applications.'
+    'This project was one of my first forays into integrating complex UI mockups into a game. I learned to develop state-heavy and animation-driven applications.'
   ]
 }
