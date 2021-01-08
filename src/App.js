753c92451f8e5bbcbd5c756ab953f8f368972498
Brandon Semilla <semibran@gmail.com>
2021-01-07 21:55:40 -0800

    space out elements

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react'
 
 export default function App () {
   return <main className='app'>
-    <h1 className='name'>Brandon Semilla</h1>
+    <div className='headings'>
+      <h1 className='name'>Brandon Semilla</h1>
+      <h2 className='title'>Web Developer</h2>
+    </div>
     <div className='socials'>
-      <img className='social -instagram' src='../assets/instagram.svg' />
       <img className='social -github' src='../assets/github.svg' />
       <img className='social -linkedin' src='../assets/linkedin.svg' />
+      <img className='social -instagram' src='../assets/instagram.svg' />
     </div>
   </main>
 }
