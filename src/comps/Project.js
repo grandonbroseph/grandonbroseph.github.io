dc17757b617d34ae04a51cf3197de89f4351364e
Brandon Semilla <semibran@gmail.com>
2021-01-07 23:50:28 -0800

    naively split styles into modules and list project lifespan

--- src/comps/Project.js
+++ src/comps/Project.js
@@ -5,17 +5,23 @@ Project.propTypes = {
   title: type.string,
   subtitle: type.string,
   images: type.arrayOf(type.string),
-  contents: type.arrayOf(type.string)
+  contents: type.arrayOf(type.string),
+  lifespan: type.arrayOf(type.number)
 }
 
-export default function Project ({ title, subtitle, images, contents }) {
+export default function Project ({ title, subtitle, lifespan, images, contents }) {
   const imageidx = 0
   const imagesrc = images[imageidx]
+  let lifestr = lifespan[0]
+  if (lifespan[1]) {
+    lifestr += '-' + lifespan[1]
+  }
   return <div className='project'>
     <div className='project-content'>
       <div className='project-headings'>
         <h3 className='project-title'>{title}</h3>
         <span className='project-subtitle'>{subtitle}</span>
+        <span className='project-lifespan'>{lifestr}</span>
       </div>
       {contents.map((content, i) =>
         <p key={i} className='project-content'>{content}</p>
