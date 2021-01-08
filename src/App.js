dcefebd2bc57bb4a6e694a54c3cf7eff59945a20
Brandon Semilla <semibran@gmail.com>
2021-01-07 21:43:55 -0800

    add some social media icons

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ export default function App () {
     <h1 className='name'>Brandon Semilla</h1>
     <div className='socials'>
       <img className='social -instagram' src='../assets/instagram.svg' />
-      <img className='social -instagram' src='../assets/instagram.svg' />
+      <img className='social -github' src='../assets/github.svg' />
+      <img className='social -linkedin' src='../assets/linkedin.svg' />
     </div>
   </main>
 }
