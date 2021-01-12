0522d7510d09878dcec950e000b2e3e8f7a3633d
Brandon Semilla <semibran@gmail.com>
2021-01-12 12:36:33 -0800

    mobile first

--- src/comps/App.js
+++ src/comps/App.js
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Project from './Project'
 import * as projects from '../projects'
 
 export default function App () {
-  return <main className='app'>
+  const [menu, setMenu] = useState(false)
+
+  function toggleMenu () {
+    setMenu(!menu)
+  }
+
+  return <main className={menu ? 'app -menu' : 'app'}>
     <div className='screen'>
+      <button onClick={toggleMenu} className='nav-toggle material-icons-round'>
+        {menu ? 'close' : 'menu'}
+      </button>
+      <nav className='nav'>
+        <ul className='nav-list'>
+          <li className='nav-item'>
+            <a className='nav-link' onClick={_ => alert('hello')}>Home</a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link'>Projects</a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link'>About</a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link'>Contact</a>
+          </li>
+        </ul>
+      </nav>
       <div className='headings'>
         <h1 className='name'>Brandon Semilla</h1>
         <h2 className='title'>Web Developer</h2>
         <h2 className='subtitle'>+ Game Designer</h2>
+        <button className='button'>View projects</button>
       </div>
       <div className='socials'>
         <img className='social -github' src='../assets/icon-github.svg' />
--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './comps/App'
 
-const root = document.getElementById('app')
-ReactDOM.render(<App/>, root, _ =>
-  root.replaceWith(root.firstChild))
+const root = document.getElementById('root')
+ReactDOM.render(<App/>, root)
